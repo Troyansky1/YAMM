737997bbbb02d409abfb228c1c8042a46ee5f680
Shira <troyansky1@mail.tau.ac.il>
2023-11-15 18:08:37 +0200

    Added basic features of repeat

--- yamm_app/lib/AddTransaction.dart
+++ yamm_app/lib/AddTransaction.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:yamm_app/Transaction.dart';
 
-
 enum RepeatOptions {
   noRepeat('Does not repeat'),
-  daily('Daily'),
-  weekly('Weekly'),
-  monthly('Monthly'),
-  yearly('Yearly');
+  daily('Day'),
+  weekly('Week'),
+  monthly('Month'),
+  yearly('Year');
 
   const RepeatOptions(this.label);
   final String label;
@@ -26,51 +25,52 @@ class _AddTransactionState extends State<AddTransaction> {
   Transaction newTransaction = Transaction();
 
   //Input controllers
-  var amountCont = TextEditingController(); 
-  var titleCont = TextEditingController(); 
+  var amountCont = TextEditingController();
+  var titleCont = TextEditingController();
   var isOutcomeCont = TextEditingController();
-  var dateCont = TextEditingController(); 
-  var serviceProviderCont = TextEditingController(); 
+  var dateCont = TextEditingController();
+  var serviceProviderCont = TextEditingController();
   var repeatOptionCont = TextEditingController();
-
+  var endDateCont = TextEditingController();
+  int? selectedRepeatEnd;
   bool repeat = false;
   bool isIncome = true;
   List<bool> _incomeOutcome = [true, false];
-  
 
-  void setIncomeOutcome(int indexToToggle){
+  void setIncomeOutcome(int indexToToggle) {
     // Presses the current button and unpresses the other button. Does nothing if button is pressed.
     int index = indexToToggle;
     setState(() {
-      if (!_incomeOutcome[index]){
-        _incomeOutcome[index] = !_incomeOutcome[index];  
-        _incomeOutcome[(index+1)%2] = !_incomeOutcome[index];  
-      } 
-    });  
-  }
-
-  void setRepeat(RepeatOptions? repeat){
-    setState(() {
-      if (repeat != RepeatOptions.noRepeat){
-        
+      if (!_incomeOutcome[index]) {
+        _incomeOutcome[index] = !_incomeOutcome[index];
+        _incomeOutcome[(index + 1) % 2] = !_incomeOutcome[index];
       }
     });
   }
 
-  
+  void setRepeat(RepeatOptions? repeat) {
+    setState(() {
+      if (repeat != RepeatOptions.noRepeat) {}
+    });
+  }
+
   @override
   void initState() {
     amountCont.text = ""; //set the initial value of text field
     super.initState();
   }
- 
- @override
+
+  @override
   Widget build(BuildContext context) {
     final List<DropdownMenuEntry<RepeatOptions>> RepeatOptionsEntries =
         <DropdownMenuEntry<RepeatOptions>>[];
     for (final RepeatOptions repeatOption in RepeatOptions.values) {
-      RepeatOptionsEntries.add(DropdownMenuEntry<RepeatOptions>(label: repeatOption.label, value: repeatOption),);}
-      
+      RepeatOptionsEntries.add(
+        DropdownMenuEntry<RepeatOptions>(
+            label: repeatOption.label, value: repeatOption),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -79,55 +79,61 @@ class _AddTransactionState extends State<AddTransaction> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[ 
+          children: <Widget>[
             Text(
               'Enter a transaction',
               textAlign: TextAlign.left,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             const TextField(
-              decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Amount'),
-              keyboardType: TextInputType.number
-            ),  
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Amount'),
+                keyboardType: TextInputType.number),
             const TextField(
-              decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Title'),
-            ),  
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Title'),
+            ),
             TextField(
               controller: dateCont,
               decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                icon: Icon(Icons.calendar_today), 
-                labelText: 'Date'),
-                readOnly: true,
-                onTap: () async{
-                  DateTime? pickedDate = await showDatePicker(
-                    context: context, 
-                    initialDate: DateTime.now(), 
-                    firstDate: DateTime(2000), 
-                    lastDate:DateTime(2101)
-                    );
-                    
-                  if(pickedDate != null ){
-                      print(pickedDate);  //pickedDate output format => 2021-03-10 00:00:00.000
-                      String formattedDate = DateFormat('dd/MM/yy').format(pickedDate); 
-                      print(formattedDate); //formatted date output using intl package =>  2021-03-16                      
-                      setState(() {
-                         dateCont.text = formattedDate; //set output date to TextField value. 
-                      });
-                  }else{
-                      print("Date is not selected");
-                  }
-                },
-            ), 
-               
+                  border: OutlineInputBorder(),
+                  icon: Icon(Icons.calendar_today),
+                  labelText: 'Date'),
+              readOnly: true,
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2125)); //end date
+
+                if (pickedDate != null) {
+                  //pickedDate output format => 2021-03-10 00:00:00.000
+                  String formattedDate =
+                      DateFormat('dd/MM/yy').format(pickedDate);
+                  //formatted date output using intl package =>  2021-03-16
+                  setState(() {
+                    dateCont.text =
+                        formattedDate; //set output date to TextField value.
+                  });
+                } else {}
+              },
+            ),
             ElevatedButton.icon(
-              
               onPressed: () {
                 showDialog(
-                  context: context, 
-                  builder: (context)=> AlertDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
                     title: Text("Repeat options"),
-                    content: 
+                    actions: <Widget>[
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          child: Text(
+                            "Repeat every:",
+                          ),
+                        ),
+                      ),
                       DropdownMenu<RepeatOptions>(
                         leadingIcon: const Icon(Icons.repeat),
                         initialSelection: RepeatOptions.noRepeat,
@@ -136,38 +142,147 @@ class _AddTransactionState extends State<AddTransaction> {
                         inputDecorationTheme: const InputDecorationTheme(
                           filled: false,
                           contentPadding: EdgeInsets.symmetric(vertical: 5.0),
+                        ),
+                        onSelected: (RepeatOptions? repeat) =>
+                            setRepeat(repeat),
                       ),
-              onSelected: (RepeatOptions? repeat) => setRepeat(repeat),
-              ), 
-                  ),);
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          child: Text(
+                            "End after:",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                          // Solves bug that the children may have unlimited space
+                          width: 500,
+                          height: 250,
+                          child: Column(
+                            children: <Widget>[
+                              ListTileTheme(
+                                horizontalTitleGap:
+                                    2, // The gap between the leading widget and title. default was 16
+                                child: RadioListTile(
+                                  title: const Text('Never'), //until end date
+                                  value: 1,
+                                  groupValue: selectedRepeatEnd,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      selectedRepeatEnd = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              ListTileTheme(
+                                horizontalTitleGap:
+                                    2, // So there is less space between radio buttin and title
+                                child: RadioListTile(
+                                  value: 2,
+                                  groupValue: selectedRepeatEnd,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      var i = selectedRepeatEnd = value;
+                                    });
+                                  },
+                                  title: ListTileTheme(
+                                    contentPadding:
+                                        EdgeInsets.symmetric(horizontal: 20),
+                                    horizontalTitleGap: 5,
+                                    child: Row(
+                                      //crossAxisAlignment: CrossAxisAlignment.stretch,
+                                      children: <Widget>[
+                                        const Text(
+                                            "On  "), // TODO find way to make the space without space char
+                                        Expanded(
+                                          child: TextField(
+                                            controller: dateCont,
+                                            decoration: const InputDecoration(
+                                                isDense: true,
+                                                border: OutlineInputBorder(),
+                                                icon:
+                                                    Icon(Icons.calendar_today),
+                                                labelText: 'Date'),
+                                            readOnly: true,
+                                            onTap: () async {
+                                              DateTime? pickedDate =
+                                                  await showDatePicker(
+                                                      context: context,
+                                                      initialDate:
+                                                          DateTime.now(),
+                                                      firstDate: DateTime(2000),
+                                                      lastDate: DateTime(
+                                                          2125)); //end date
+                                            },
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              ListTileTheme(
+                                horizontalTitleGap:
+                                    2, // So there is less space between radio buttin and title
+                                child: RadioListTile(
+                                  value: 2,
+                                  groupValue: selectedRepeatEnd,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      var i = selectedRepeatEnd = value;
+                                    });
+                                  },
+                                  title: const ListTileTheme(
+                                    contentPadding:
+                                        EdgeInsets.symmetric(horizontal: 20),
+                                    horizontalTitleGap: 5,
+                                    child: Row(
+                                      //crossAxisAlignment: CrossAxisAlignment.stretch,
+                                      children: <Widget>[
+                                        Text(
+                                            "After  "), // TODO find way to make the space without space char
+                                        Expanded(
+                                          child: TextField(
+                                            maxLength: 3,
+                                            decoration: InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                labelText: ''),
+                                            keyboardType: TextInputType.number,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ))
+                    ],
+                  ),
+                );
               },
-              icon: const Icon( Icons.repeat, size: 24.0, ),
+              icon: const Icon(
+                Icons.repeat,
+                size: 24.0,
+              ),
               label: const Text('Repeat'), // <-- Text
-            ),  
-            
-              
+            ),
             ToggleButtons(
-              constraints: BoxConstraints(minWidth: (MediaQuery.of(context).size.width - 36) / 3),              
+              constraints: BoxConstraints(
+                  minWidth: (MediaQuery.of(context).size.width - 36) / 3),
               onPressed: (int index) => setIncomeOutcome(index),
               isSelected: _incomeOutcome,
-            borderRadius: BorderRadius.circular(40),
-            borderWidth: 5,
-            children: const [
-              Text("Income"),
-              Text("Outcome")
-            ],
+              borderRadius: BorderRadius.circular(40),
+              borderWidth: 5,
+              children: const [Text("Income"), Text("Outcome")],
             ),
-                   
             const TextField(
-              decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Service provider'),
-            ),         
-
-
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Service provider'),
+            ),
           ],
         ),
       ),
-      
     );
   }
 }
-
